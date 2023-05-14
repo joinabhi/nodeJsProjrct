9504aeae04c076ceff8ee2fdb4d57e096ededcab
joinabhi <abhi.gpt96@gmail.com>
2023-05-14 16:21:45 +0530

    add file1

--- hello.js
+++ hello.js
@@ -1,10 +1,25 @@
-const http=require("http");
+// const http=require("http");
+// const server=http.createServer((req,res)=>{
+//     res.end("Hello Mr. Abhishek Gupta, aur gandu");
+// })
+// server.listen(8000, "127.0.0.1", ()=>{
+//     console.log("listening to the port no. 8000")
+// })
+
+const http=request('http');
 const server=http.createServer((req,res)=>{
-    res.end("Hello Mr. Abhishek Gupta, aur gandu");
+    console.log(req.url, req.method, req.header)
+    res.setHeader('content-type','text/html')
+    res.write('<html>')
+    res.write('<head><title>My First Page</head></title>')
+    res.write('<body><h1>Hello from my hello.js server</body></h1>')
+    res.write('</html>')
+    res.end("Hello Everyone");
 })
-server.listen(8000, "127.0.0.1", ()=>{
-    console.log("listening to the port no. 8000")
-})
+
+server.listen(3000)
+
+
 
 
 
