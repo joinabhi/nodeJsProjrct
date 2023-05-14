b79365c676e96f75cb571f1f85a1d387860a0ef4
joinabhi <abhi.gpt96@gmail.com>
2023-05-14 13:25:35 +0530

    add new server file

--- hello.js
+++ hello.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+const http=require("http");
+const server=http.createServer((req,res)=>{
+    res.end("Hello Mr. Abhishek Gupta, aur gandu");
+})
+server.listen(8000, "127.0.0.1", ()=>{
+    console.log("listening to the port no. 8000")
+})
+
+
+
