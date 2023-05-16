44336c0f1ac098823bf99ba6f9599c9673217871
joinabhi <abhi.gpt96@gmail.com>
2023-05-16 14:23:53 +0530

    add new file THE ROCK

--- hello.js
+++ hello.js
@@ -1,65 +1,10 @@
-// const http=require("http");
-// const server=http.createServer((req,res)=>{
-//     res.end("Hello Mr. Abhishek Gupta, aur gandu");
-// })
-// server.listen(8000, "127.0.0.1", ()=>{
-//     console.log("listening to the port no. 8000")
-// })
-
 const http=require('http');
-const fs=require('fs');
-const server=http.createServer((req,res)=>{
-    // console.log(req)
-    // res.setHeader('content-type','text/html')
-    const url=req.url;
-    const method=res.method
-    if(url==='/'){
+const routes= require('./routes');
+const server=http.createServer(routes);
 
-        fs.readFile('message.txt', {encoding:"utf-8"},(err, data)=>{
-            if(err){
-                console.log(err)
-            }
-            console.log(`data from file`+ data)
-            res.write('<html>')
-            res.write('<head><title>My First Page</title></head>')
-            res.write(`<body>${data}</body>`)
-            res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">SEND</button></form></body>')
-            res.write('</html>')
-            return res.end()  
-        })
-       
-    }
-    if(url==='/message' && method==='POST'){
-        const body=[]
-        req.on('data',(chunk)=>{
-            console.log(chunk)
-            body.push(chunk)
-        })
-        req.on('end',()=>{
-            const parsedbody=Buffer.concat(body).toString();
-          
-            console.log(parsedbody)
-            const message=parsedbody.split('=')[1];
-            fs.writeFileSync('message.txt', message, err =>{
-                res.statusCode=302
-                res.setHeader('Location','/');
-                return res.end()
-            });
-        
-        })
-       }
-       else{
-        res.setHeader('content-type','text/html');
-        res.write('<html>')
-        res.write('<head><title>My First Page</title></head>')
-        res.write('<body><h1>Hello to my hello.js server</h1></body>')
-        res.write('</html>')
-        res.end();
-       }
-    
-})
-
-server.listen(4444)
+console.log(routes.someText);
+   
+server.listen(3000)
 
 
 
